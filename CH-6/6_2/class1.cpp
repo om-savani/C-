a4eb03cab8a70b8617bcf1003393f08870c30f9e
Om Savani <156919604+om-savani@users.noreply.github.com>
2024-05-02 12:38:51 +0530

    CPP

---
+++ CH-6/6_2/class1.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+
+using namespace std;
+
+class A
+{
+protected:
+	int a;
+};
+
+class B : public A
+{
+protected:
+	int b;
+
+public:
+	B()
+	{
+		cout << "Enter A : ";
+        cin >> a;
+		cout << "Enter B : ";
+        cin >> b;
+	}
+};
+
+class C : public A
+{
+protected:
+	int c;
+
+public:
+	C()
+	{
+		cout << "Enter C : ";
+        cin >> c;
+	}
+};
+
+class D : public B, public C
+{
+protected:
+    int d,sum=0;
+
+public:
+	D()
+	{
+		cout << "Enter D : ";
+        cin >> d;
+
+		sum = B::a + b + c + d;
+        cout << endl << "Sum = " << sum << endl;
+	}
+
+};
+
